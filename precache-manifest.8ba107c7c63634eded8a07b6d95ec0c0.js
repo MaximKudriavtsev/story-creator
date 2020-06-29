5d0f4b843bb3dccd91af142764017efed012ef1b
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-06-29 17:53:33 +0300

    Add site

---
+++ precache-manifest.8ba107c7c63634eded8a07b6d95ec0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce721657ff9088fbe977ca2787f0b04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c247eefb624140c50b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1173fb90d5a75467e564",
+    "url": "/static/js/2.1116bd7e.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/static/js/2.1116bd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c247eefb624140c50b",
+    "url": "/static/js/main.305362c7.chunk.js"
+  },
+  {
+    "revision": "0debe939d1b934240835",
+    "url": "/static/js/runtime-main.037661b2.js"
+  }
+]);
