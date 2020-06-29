b02e4796d13f8d9cbb7baa0755ac7acb5f27f203
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-06-29 19:05:30 +0300

    Updates

---
+++ precache-manifest.ddae5ebbc23d14e57078b1201e22f925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1040a2df59f7191c7bc303fdf8394cd",
+    "url": "/story-creator/index.html"
+  },
+  {
+    "revision": "2c2d923e0b8e031fbbb5",
+    "url": "/story-creator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1173fb90d5a75467e564",
+    "url": "/story-creator/static/js/2.1116bd7e.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/story-creator/static/js/2.1116bd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2d923e0b8e031fbbb5",
+    "url": "/story-creator/static/js/main.eefb2588.chunk.js"
+  },
+  {
+    "revision": "c8106f8f7c09e7790741",
+    "url": "/story-creator/static/js/runtime-main.9a939460.js"
+  }
+]);
