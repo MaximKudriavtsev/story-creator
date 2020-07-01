d232e9f9e3e29c36301780528ddecea1f080d0c2
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-07-01 16:08:44 +0300

    Add markdown parcer

--- src/export.js
+++ src/export.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
+import marked from 'marked';
 
 // { name: 'id', title: '#id' },
 // { name: 'role', title: 'Role' },
@@ -19,6 +20,7 @@ const generateMD = (data) => {
 
   result.push('\n#### Acceptance Criteria & Tests');
   result.push('\n| ID | Criteria | Test |');
+  result.push('| -- | :------- | :--- |');
 
   data.forEach((story, id) => {
     const tableLine = `| ${id + 1} | Capability to ${story.action}. | ${story.tests.map((test, testId) => `${testId + 1}. ${test.text}. `).join('')} |`;
@@ -30,22 +32,25 @@ const generateMD = (data) => {
 
 export default () => {
   const stories = useSelector(state => state.stories);
+  const markdown = generateMD(stories);
 
   const exportMD = React.useCallback(() => {
     const filename = "data.md";
-    const mdData = generateMD(stories);
 
-    const blob = new Blob([mdData], {
+    const blob = new Blob([markdown], {
       type: 'text/plain;charset=utf-8'
     });
 
     saveAs(blob, filename);
-  }, [stories]);
+  }, [markdown]);
 
   return (
     <React.Fragment>
         <Paper>
-          <TextField value={generateMD(stories)} variant="outlined" multiline style={{ width: '100%' }} />
+          <TextField value={markdown} variant="outlined" multiline style={{ width: '100%' }} />
+        </Paper>
+        <Paper style={{ marginTop: '20px' }}>
+          <p style={{ padding: '12px' }} dangerouslySetInnerHTML={{__html: marked(markdown) }} />
         </Paper>
       <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '14px' }}>
         <Button variant="outlined" color="primary" onClick={exportMD}>
