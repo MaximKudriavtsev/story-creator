72e1654f6bd426320c0e73e2fd58a09fc91d1334
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-07-25 22:45:18 +0300

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/story-creator",
   siteMetadata: {
     title: `Story Creator App`,
     description: `The application generates user stories MD markup based on your minimum data.`,
@@ -27,8 +28,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-140052799-2',
+      },
+    },
   ],
 }
