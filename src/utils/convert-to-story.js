0c7ea5fba1c1dc434fbd5d805c24401777890ea1
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-08-08 23:10:29 +0300

    Fix bug with articles

--- src/utils/convert-to-story.js
+++ src/utils/convert-to-story.js
@@ -1,1 +1,5 @@
-export const convertToStory = story => `As a ${story.role.trim()}, I want to be able to ${story.action.trim()} so that I can ${story.purpose.trim()}.`;
+const vowels = ['a', 'e', 'i', 'o', 'y'];
+const isVowel = (char = '') => vowels.indexOf(char.toLowerCase()) !== -1;
+
+export const convertToStory = story =>
+  `As ${isVowel(story.role[0]) ? 'an' : 'a'} ${story.role.trim()}, I want to be able to ${story.action.trim()} so that I can ${story.purpose.trim()}.`;
